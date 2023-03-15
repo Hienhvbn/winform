745fd158a7ce36931b5f7b694945b1cd565f25ea
cong <cong@DESKTOP-1COAG34>
2023-03-15 13:00:22 +0700

    ádgfdhg

--- frmQuanLySach.Designer.cs
+++ frmQuanLySach.Designer.cs
@@ -156,6 +156,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(4, 207);
             this.dataGridView1.Name = "dataGridView1";
