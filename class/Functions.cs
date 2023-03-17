986e25d52b622bcc4746030072c29d69b90990d3
Hienhvbn <duonghien491@gmail.com>
2023-03-18 00:04:12 +0700

    .

--- class/Functions.cs
+++ class/Functions.cs
@@ -16,12 +16,12 @@ namespace quan_li_ban_sach.Class
         public static void Connect()
         {
             Con = new SqlConnection();   //Khởi tạo đối tượng
-            Con.ConnectionString = "Server = DESKTOP-1COAG34; Database = qlbs ;Integrated Security = True";
+            Con.ConnectionString = "Server = VHien03; Database = qlbs ;Integrated Security = True";
             Con.Open();                  //Mở kết nối
             //Kiểm tra kết nối
             if (Con.State != ConnectionState.Open)
-            //MessageBox.Show("Kết nối thành công");
-            MessageBox.Show("Không thể kết nối với dữ liệu");
+                //MessageBox.Show("Kết nối thành công");
+                MessageBox.Show("Không thể kết nối với dữ liệu");
 
         }
         public static void Disconnect()
@@ -72,10 +72,10 @@ namespace quan_li_ban_sach.Class
             SqlDataAdapter dap = new SqlDataAdapter(sql, Con);
             DataTable table = new DataTable();
             dap.Fill(table);
-            if(table.Rows.Count > 0)
+            if (table.Rows.Count > 0)
                 return true;
             else return false;
-            
+
         }
         //Hàm thực hiện câu lệnh SQL
         public static void RunSQL(string sql)
@@ -106,11 +106,78 @@ namespace quan_li_ban_sach.Class
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Dữ liệu đang được dùng, không thể xoá...", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //MessageBox.Show("Dữ liệu đang được dùng, không thể xoá...", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                 MessageBox.Show(ex.ToString());
             }
             cmd.Dispose();
             cmd = null;
+        }
+        public static string CreateKey(string tiento)
+        {
+            string key = tiento;
+            string[] partsDay;
+            partsDay = DateTime.Now.ToShortDateString().Split('/');
+            //Ví dụ 07/08/2009
+            string d = String.Format("{0}{1}{2}", partsDay[0], partsDay[1], partsDay[2]);
+            key = key + d;
+            string[] partsTime;
+            partsTime = DateTime.Now.ToLongTimeString().Split(':');
+            //Ví dụ 7:08:03 PM hoặc 7:08:03 AM
+            if (partsTime[2].Substring(3, 2) == "PM")
+                partsTime[0] = ConvertTimeTo24(partsTime[0]);
+            if (partsTime[2].Substring(3, 2) == "AM")
+                if (partsTime[0].Length == 1)
+                    partsTime[0] = "0" + partsTime[0];
+            //Xóa ký tự trắng và PM hoặc AM
+            partsTime[2] = partsTime[2].Remove(2, 3);
+            string t;
+            t = String.Format("_{0}{1}{2}", partsTime[0], partsTime[1], partsTime[2]);
+            key = key + t;
+            return key;
+        }
+        public static string ConvertTimeTo24(string hour)
+        {
+            string h = "";
+            switch (hour)
+            {
+                case "1":
+                    h = "13";
+                    break;
+                case "2":
+                    h = "14";
+                    break;
+                case "3":
+                    h = "15";
+                    break;
+                case "4":
+                    h = "16";
+                    break;
+                case "5":
+                    h = "17";
+                    break;
+                case "6":
+                    h = "18";
+                    break;
+                case "7":
+                    h = "19";
+                    break;
+                case "8":
+                    h = "20";
+                    break;
+                case "9":
+                    h = "21";
+                    break;
+                case "10":
+                    h = "22";
+                    break;
+                case "11":
+                    h = "23";
+                    break;
+                case "12":
+                    h = "0";
+                    break;
+            }
+            return h;
         }
     }
 }
